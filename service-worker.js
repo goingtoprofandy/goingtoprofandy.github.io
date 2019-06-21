1663690519b03d4c8751611695bd440981c08df3
Andy Maulana Yusuf <goingtoprofandy@gmail.com>
2019-06-21 16:34:28 +0700

    Initial Commit

---
+++ service-worker.js
@@ -0,0 +1,84 @@
+importScripts('/workbox-sw.js');
+
+const CACHE_NAME = "footballApps";
+var urlsToCache = [
+  {url: "/", revision: "1"},
+  {url: "/index.html", revision: "1"},
+  {url: "/component/navigation.html", revision: "1"},
+  {url: "/pages/klasemen.html", revision: "1"},
+  {url: "/pages/team.html", revision: "1"},
+  {url: "/pages/detail.html", revision: "1"},
+  {url: "/pages/about.html", revision: "1"},
+  {url: "/pages/contact-us.html", revision: "1"},
+  {url: "/assets/css/materialize.min.css", revision: "1"},
+  {url: "/assets/image/icon.png", revision: "1"},
+  {url: "/assets/image/icon-72x72.png", revision: "1"},
+  {url: "/assets/image/icon-96x96.png", revision: "1"},
+  {url: "/assets/image/icon-128x128.png", revision: "1"},
+  {url: "/assets/image/icon-144x144.png", revision: "1"},
+  {url: "/assets/image/icon-152x152.png", revision: "1"},
+  {url: "/assets/image/icon-192x192.png", revision: "1"},
+  {url: "/assets/image/icon-384x384.png", revision: "1"},
+  {url: "/assets/image/icon-512x512.png", revision: "1"},
+  {url: "/assets/js/materialize.min.js", revision: "1"},
+  {url: "/assets/js/component.js", revision: "1"},
+  {url: "/assets/js/idb.js", revision: "1"},
+  {url: "/manifest.json", revision: "1"},
+  {url: "/workbox-sw.js", revision: "1"}
+];
+
+workbox.precaching.precacheAndRoute(urlsToCache);
+
+// self.addEventListener('install', function(event){
+// 	event.waitUntil(
+// 		caches.open(CACHE_NAME)
+// 		.then(function(cache) {
+// 			return cache.addAll(urlsToCache);
+// 		})
+// 	);
+// })
+
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					if(cacheName != CACHE_NAME){	
+						console.log("ServiceWorker: cache " + cacheName + " dihapus");
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+})
+
+workbox.routing.registerRoute(
+	new RegExp('/'),
+	workbox.strategies.staleWhileRevalidate({
+		cacheName: CACHE_NAME
+	})
+);
+
+// Add Push Playload
+self.addEventListener('push', function(event) {
+	var body;
+	if (event.data) {
+		body = event.data.text();
+	} else {
+		body = 'Push message no payload';
+	}
+	var options = {
+		body: body,
+		icon: '/assets/image/icon-72x72.png',
+		vibrate: [100, 50, 100],
+		data: {
+		dateOfArrival: Date.now(),
+		primaryKey: 1
+		}
+	};
+	event.waitUntil(
+		self.registration.showNotification('Push Notification', options)
+	);
+});
